c5d3c1a5d214d928b77e953d2da165f1110a7049
Greg <Ghughes610@gmail.com>
2021-06-09 11:07:25 -0600

    commit tree 5ef5d7bd060d4d3c46a54cc39688f9d496fb7747

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,33 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = data[season].find {  |contestant| contestant["status"] == "Winner" }
+  winner["name"].split[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  women = data.values.flatten.find {  |contestant| contestant["occupation"] == occupation}
+  women["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  data.values.flatten.reduce(0) do  |sum, contestant|
+    if contestant["hometown"] == hometown
+      sum + 1
+    else
+      sum
+    end
+  end
 end
 
 def get_occupation(data, hometown)
-  # code here
+  winner = data.values.flatten.find {  |contestant| contestant["hometown"] == hometown}
+  winner["occupation"]
 end
 
 def get_average_age_for_season(data, season)
   # code here
 end
+
+
+    
