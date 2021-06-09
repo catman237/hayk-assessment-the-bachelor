29454d426d3900928e67f9c3a85b98e3fb73f08a
Greg <Ghughes610@gmail.com>
2021-06-09 11:32:23 -0600

    done

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,37 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = data[season].find {  |contestant| contestant["status"] == "Winner" }
+  winner["name"].split[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  women = data.values.flatten.find {  |contestant| contestant["occupation"] == occupation }
+  women["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  data.values.flatten.reduce(0) do  |sum, contestant|
+    if contestant["hometown"] == hometown
+      sum + 1
+    else
+      sum
+    end
+  end
 end
 
 def get_occupation(data, hometown)
-  # code here
+  # how can i use this without a var
+  winner = data.values.flatten.find {  |contestant| contestant["hometown"] == hometown }
+  winner["occupation"]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total_ages = data[season].reduce(0) do  |count, contestant| 
+    count + contestant["age"].to_f
+  end
+  (total_ages/data[season].length).round
 end
+
+
+    
